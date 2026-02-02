74985f76de4c6da98d9608f469c94165e56c1dd9
Numpad73192846 <seongjun3262@gmail.com>
2026-02-03 05:08:58 +0900

    fix: TutorAvailability 중복 클래스 정의 제거

--- teamproject/src/main/java/com/aloha/teamproject/dto/TutorAvailability.java
+++ teamproject/src/main/java/com/aloha/teamproject/dto/TutorAvailability.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.aloha.teamproject.dto;
 
 import java.time.LocalDateTime;
@@ -40,46 +39,3 @@ public class TutorAvailability {
         OPEN, BOOKED, CANCELLED
     }
 }
-=======
-package com.aloha.teamproject.dto;
-
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-@AllArgsConstructor
-public class TutorAvailability {
-
-    @JsonIgnore
-    private Long no;
-    @JsonIgnore
-    private String userId;
-    @Builder.Default
-    private String id = UUID.randomUUID().toString();
-    private LocalDateTime startAt;
-    private LocalDateTime endAt;
-    @Builder.Default
-    private Status status = Status.OPEN;
-    @JsonIgnore
-    private Date createdAt;
-    @JsonIgnore
-    private Date updatedAt;
-
-    public TutorAvailability() {
-        this.id = UUID.randomUUID().toString();
-        this.status = Status.OPEN;
-    }
-
-    public enum Status {
-        OPEN, BOOKED, CANCELLED
-    }
-}
->>>>>>> origin/cheshire
